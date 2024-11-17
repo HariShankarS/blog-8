0795826e1755f8c5fe09199d10ee942d46240ee8
HariShankarS <s.harishankar333@gmail.com>
2024-11-17 23:46:18 +0530

    No hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts << "."
+  config.hosts.clear
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
